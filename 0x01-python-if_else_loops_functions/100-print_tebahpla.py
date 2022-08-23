48894a3fff351f1f2baa1846546afefca56db2bc
Simeon Mustapha <simeon.mustapha@gmail.com>
2022-08-23 07:06:02 -0700

    commit

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+i = 0
 for c in range(ord('z'), ord('a') - 1, -1):
-    print("{:c}".format((c - (ord('a') - ord('A'))) if c % 2 else c), end='')
-
+    print("{}".format(chr(c - i)), end="")
+    i = 32 if i == 0 else 0
