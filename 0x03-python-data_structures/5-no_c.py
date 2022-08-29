ce5cf83175a392dc0fbb818b1a950067b9725395
Simeon Mustapha <simeon.mustapha@gmail.com>
2022-08-28 19:05:25 -0700

    commit

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-
-
 def no_c(my_string):
-    copy_str = [x for x in my_string if x != 'c' or x != 'C']
-    return ("".join(copy_str))
+    new_string = ""
+    for c in my_string:
+        if c != 'c' and c != 'C':
+            new_string += c
+    return new_string
